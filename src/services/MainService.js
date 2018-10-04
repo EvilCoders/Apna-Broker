ccda4bccc9aa88b874ea738c3a58b68868d62aac
rishav-pandey <27219302+rishav-pandey@users.noreply.github.com>
2018-10-05 04:20:48 +0530

    added loader, flag for disable button on click and reset price on method call

--- src/services/MainService.js
+++ src/services/MainService.js
@@ -14,10 +14,12 @@
   function PredictRentService ($http) {
     const RentSrvc = this;
 
+    RentSrvc.baseUrl = 'http://192.168.43.124:5000';
+
     RentSrvc.getResult = function (item, response) {
       return $http({
         method: 'POST',
-        url: 'http://192.168.43.124:5000/api',
+        url: RentSrvc.baseUrl + '/api',
         data: item
       })
     }
